d5ebd1013b1f387dd17f71f9ac196353ca71847a
Fouchimi <ousmanefouchimi@gmail.com>
2017-05-19 17:59:56 -0600

    modified index.j

--- index.js
+++ index.js
@@ -25,10 +25,10 @@ var api = new ParseServer({
   appId: process.env.APP_ID || '3L3nrFqVD2Ilg_6RXuMusgVba0_uARi6E',
   masterKey: process.env.MASTER_KEY || 'sMVHq9lgSmQUtNxyEdsBJaUvZUkqB7eKg', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://yourturnapp.herokuapp.com/parse',  // Don't forget to change to https if needed
+  push: pushConfig,
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  },
-  push : pushConfig
+  }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
