4f684c4d78d4d6c9cd0fbf125d6a4e4fbe6424ba
Fouchimi <ousmanefouchimi@gmail.com>
2017-05-22 12:33:58 -0600

    modify cloud.js file

--- cloud/main.js
+++ cloud/main.js
@@ -1,15 +1,24 @@
 
 Parse.Cloud.define('pushChannel', function(request, response) {
   var params = request.params;
-  var message = params.alert;
-  var title = params.title;
-  var receiverId = params.receiverId;
+  var senderName = params.senderName;
+  var senderId = params.senderId;
+  var message = params.message;
+  var recipients = params.recipients;
+  var friendsNumber = params.count;
 
   
   var pushQuery = new Parse.Query(Parse.Installation);
   pushQuery.equalTo("deviceType", "android");
+  //containedIn
+  if(friendsNumber > 1) {
+     var[] friendList = recipients.split(",");
+     pushQuery.containedIn("device_id", friendList);
+  }else {
+     pushQuery.equalTo("device_id", recipients);
 
-  var payload = {"data": {"alert": message, "title": title} };
+  }
+  var payload = {"data": {"alert": message, "title": senderName + " send you a message" } };
 
   Parse.Push.send({
   where: pushQuery,
