e3732a7b93b7b73ade34841cd2264eab76e748aa
Fouchimi <ousmanefouchimi@gmail.com>
2017-05-29 16:08:25 -0600

    change payload when friendcount goes beyond more than one friend

--- cloud/main.js
+++ cloud/main.js
@@ -1,34 +1,62 @@
 Parse.Cloud.define('senderChannel', function(request, response) {
   var params = request.params;
   var senderId = params.senderId;
-  var sharedValue = params.sharedValue;
+  var sharedValue = params.sharedValueList;
   var recipientList = params.recipientList;
   var friendCount = params.friendCount;
 
 var pushQuery = new Parse.Query(Parse.Installation);
 pushQuery.equalTo("deviceType", "android");
 
+var payloadList = [];
+
 if(friendCount > 1) {
    var friendList = recipientList.split(',');
-   var friendListArray = [];
+   var valueList = sharedValue.split(',');
+   var friendListArray = valueListArray =  [];
 
    for(var item in friendList) friendListArray.push(item);
 
+   for(var item in valueList) valueListArray.push(item);
+
+   for(int i= 0; i < valueListArray.length; i++) {
+     var currentPayload = {"title": senderId, "alert": valueListArray[i]};
+     payloadList.push(currentPayload);
+   }
+
    pushQuery.containedIn("device_id", friendListArray);
 
-} else pushQuery.equalTo("device_id", recipientList);
+   valueListArray.forEach(function(listItem, index){
 
-var payload = {"title": senderId, "alert": sharedValue};
+     Parse.Push.send({
+       where: pushQuery,
+       data: payload,
+     }, { success: function() {
+        console.log("#### PUSH OK");
+     }, error: function(error) {
+        console.log("#### PUSH ERROR" + error.message);
+     }, useMasterKey: true});
+     response.success('success');
+     
+   });
 
-  Parse.Push.send({
-    where: pushQuery,
-    data: payload,
-  }, { success: function() {
-     console.log("#### PUSH OK");
-  }, error: function(error) {
-     console.log("#### PUSH ERROR" + error.message);
-  }, useMasterKey: true});
-  response.success('success');
+} else {
+
+  pushQuery.equalTo("device_id", recipientList);
+
+  var payload = {"title": senderId, "alert": sharedValue};
+
+      Parse.Push.send({
+        where: pushQuery,
+        data: payload,
+      }, { success: function() {
+         console.log("#### PUSH OK");
+      }, error: function(error) {
+         console.log("#### PUSH ERROR" + error.message);
+      }, useMasterKey: true});
+      response.success('success');
+  }
+
 });
 
 
